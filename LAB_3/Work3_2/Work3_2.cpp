d8326ca704bb2e8a40efe36b0b0c08a82bc93f65
DenisDerX <densichanell@gmail.com>
2022-11-04 19:36:57 +0200

    Hallo

--- LAB_3/Work3_2/Work3_2.cpp
+++ LAB_3/Work3_2/Work3_2.cpp
@@ -18,4 +18,25 @@ int main()
 {
 	system("cls");
 	setlocale(LC_ALL, "Ukr");
-	
+	double mult = 1.0, sum = 1, i, j, i_start, i_end, j_start, j_end;
+	cout << "Введіть початкове значення i= ";
+	cin >> i_start;
+	cout << "Введіть кінцеве значення i= ";
+	cin >> i_end;
+	cout << "Введіть початкове значення j= ";
+	cin >> j_start;
+	cout << "Введіть кінцеве значення j= ";
+	cin >> j_end;
+
+	for (i = i_start; i <= i_end; i++)
+	{
+		sum = 0;
+		for (j = j_start; j <= j_end; j++)
+		{
+			sum = sum + (i / (i + 1));
+		}
+		mult = mult * (sum / sqrt(j));
+	}
+	cout << "Результат = " << mult << endl;
+	return 0;
+}
