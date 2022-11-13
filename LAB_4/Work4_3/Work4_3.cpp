b81cf401574fe575f0633240485187f94b331e62
DenisDerX <densichanell@gmail.com>
2022-11-13 13:50:07 +0200

    Hallo

---
+++ LAB_4/Work4_3/Work4_3.cpp
@@ -0,0 +1,34 @@
+﻿#include <math.h>
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	system("cls");
+	setlocale(LC_ALL, "Ukr");
+	const int A = 1;
+	double x{}, y, xstart, xend, xstep;
+	cout << "Введіть інтервал xstart..xend\n";
+	cout << "Введіть xstart =";
+	cin >> xstart;
+	cout << "Введіть xend =";
+	cin >> xend;
+	cout << "Введіть xstep =";
+	cin >> xstep;
+	printf("_________________________\n");
+	printf("| x        | y        |\n");
+	printf("-------------------------\n");
+	while (x <= xend)
+	{
+		x = x + xstep;
+		y = (cos(pow(x, 3) + pow(x, 2) - pow(x,1) + A)) * exp(2 * x);
+		printf("| %8.4f | %8.4f |\n", x, y);
+	}
+	
+	printf("-------------------------\n");
+	system("pause");
+	return 0;
+}
+
