57f09244068d83d118489bb2a28b97857a6a75f1
DenisDerX <densichanell@gmail.com>
2022-11-14 11:34:15 +0200

    Hallo

---
+++ LAB_4/Work4_2/Work4_2.cpp
@@ -0,0 +1,24 @@
+﻿#include <math.h>
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	system("cls");
+	setlocale(LC_ALL, "Ukr");
+	int n, a=0, b=0;
+	cout << "Введіть ціле число =";
+	cin >> n;
+	while (n>0)
+	{
+		if ((n%10)%2 == 0)
+			a++;
+		else b++;
+		n /= 10;
+	}
+	printf("Парні числа= %d\n",a);
+	printf("Непарні числа = %d\n", b);
+	return 0;
+}
