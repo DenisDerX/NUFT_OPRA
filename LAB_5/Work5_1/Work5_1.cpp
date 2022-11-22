c48d9c471c1b1692f0eb4569fbf93e33b612712c
Denys Derthiy <densichanell@gmail.com>
2022-11-22 18:01:44 +0200

    Hallo

--- LAB_5/Work5_1/Work5_1.cpp
+++ LAB_5/Work5_1/Work5_1.cpp
@@ -8,7 +8,7 @@ int main()
 {
 	system("cls");
 	setlocale(LC_ALL, "Ukr");
-	const double A = 2.51, B=5.32, C=2;
+	const double C = 2;
 	double sum, mul, P;
 	int x, k, i, i_start, i_end, k_start, k_end;
 	cout << "Введіть початкове значення i= ";
@@ -27,20 +27,18 @@ int main()
 	{
 		k++;
 		sum = sum * k * sin(k);
-	} 
-	while (k <= k_end);
+	} while (k <= k_end);
 	mul = 1;
 	i = i_start + 1;
 	do
 	{
 		i++;
-		mul = mul * ((pow(sin(x), 2)) * i + C * (exp(-1*x + i)));
-	} 
-	while (i <= i_end);
-	P = sqrt(B * A)*sum+mul;
+		mul = mul * ((pow(sin(x), 2)) * i + C * (exp(-1 * x + i)));
+	} while (i <= i_end);
+	P = sqrt(5.32 * 2.51) * sum + mul;
 	cout << "Сума= " << sum << endl;
 	cout << "Добуток= " << mul << endl;
-	printf("P=%7.0f\n",P);
+	printf("P=%7.0f\n", P);
 	system("pause");
 	return(0);
 }
