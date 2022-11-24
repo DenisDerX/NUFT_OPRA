da90710f1c791d59c97b78fb3c943a00822f58fe
Denys Derthiy <densichanell@gmail.com>
2022-11-24 18:42:02 +0200

    Hallo

---
+++ LAB_5/Work5_4/Work5_4.cpp
@@ -0,0 +1,30 @@
+﻿#include <math.h>
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+
+using namespace std;
+
+int F(int i)
+{
+    if (i == 0)
+        return 0;
+    if (i == 1)
+        return 1;
+    return F(i - 1) + F(i - 2);
+}
+int main()
+{
+    system("cls");
+    setlocale(LC_ALL, "Ukr");
+    int x;
+    cout << "Вкажіть кількість чиесл які бажаєте, щоб було виведено на екран: ";
+    cin >> x;
+    int i = 0;
+    while (i < x)
+    {
+        cout << F(i) << " ";
+        i++;
+    }
+    return 0;
+}
