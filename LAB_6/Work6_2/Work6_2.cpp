6cbd0bb4bc75291a45ea31c673b959d42a92e3b4
Denys Derthiy <densichanell@gmail.com>
2022-11-30 18:22:30 +0200

    Hallo

---
+++ LAB_6/Work6_2/Work6_2.cpp
@@ -0,0 +1,44 @@
+﻿#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    system("cls");
+    setlocale(LC_ALL, "Ukr");
+    const int n = 10;
+    int i, im, k;
+    double a[n], max;
+    cout << "Введіть масив А["<<n<<"]= ";
+    for (i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    system("cls");
+    cout<< " Вхідний масив А["<<n<<"]= ";
+    for (i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    im = 0;
+    for (i = 1; i < n; i++)
+    {
+        im = i;
+    }
+    cout << "Вивести всі числа масиву менші за = ";
+    cin >> k;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] < k)
+        {
+            cout << a[i] << " ";
+        }
+    }
+    cout << endl << "Індекс= " << im << endl;
+    system("pause");
+    return 0;
+}
+
+
