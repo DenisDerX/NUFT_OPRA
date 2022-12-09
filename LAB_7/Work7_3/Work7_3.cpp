ca3ff3be4f1c60c9ee5bb578df6dbe2e6768a61f
Denys Derthiy <densichanell@gmail.com>
2022-12-09 08:39:06 +0200

    Hallo

---
+++ LAB_7/Work7_3/Work7_3.cpp
@@ -0,0 +1,46 @@
+﻿#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    system("cls");
+    setlocale(LC_ALL, "Ukr");
+    const int n = 7, m = 8;
+    int i, j, min;
+    int mat[n][m];
+    srand(time(NULL));
+    printf("Ваша матриця");
+    cout << endl;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            mat[i][j] = rand() % 100 - 50;
+            printf("%4d", mat[i][j]);
+        }
+        cout << endl;
+    }
+	for (int i = 0; i < n; i++)
+	{
+		int k = 0;
+		double a = 0;
+		for (int j = 0; j < m; j++)
+		{
+			if (j >= i && i <= n - 1 && j <= m - 1 - i)
+			{
+				a = a + mat[i][j];
+				k++;
+			}
+		}
+		if (k!= 0)
+		{
+			a = a / k;
+			cout << a << " ";
+		}
+	}
+
+	return 0;
+}
---
+++ LAB_8/Work8_1/Work8_1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include<string>
+#include<stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Ukr");
+	char s;
+	string stroka;
+	int k = 0, i;
+	cout << "Введіть текст: ";
+	getline(cin, stroka);
+	cout << "Введіть символ, яки бажаєте шукати: ";
+	cin >> s;
+	cout << "Введіть початкову точку, з якої бажаєте здійснити пошук: ";
+	cin >> i;
+	for (i; i < stroka.size(); i++)
+	{
+		if (stroka[i] == s)
+		{
+			k++;
+		}
+	}
+	cout << "Кількість знайдених символів: " << k << endl;
+	return 0;
+}
