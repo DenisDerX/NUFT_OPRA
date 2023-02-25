b62d5b418be133c03c15098ab18bcbf7d15e67e7
Denys Derthiy <densichanell@gmail.com>
2023-02-25 19:13:05 +0200

    Hallo

--- SM_2/LAB_10/Work10_1/Work10_1.cpp
+++ SM_2/LAB_10/Work10_1/Work10_1.cpp
@@ -12,25 +12,27 @@ int main()
     srand(time(NULL));
     const int n = 10;
     int arr[n];
-    int max = 0, index = 0;
+    int *max;
+    int index = 0;
     cout << "Згенерований масив: ";
     for (int i = 0; i < n; i++)
     {
         arr[i] = rand() % 100 - 40;
         cout << arr[i] << " ";
     }
-    cout <<endl;
+    cout << endl;
+    max= &arr[0];
     for (int i = 0; i < n; i++) {
         if (arr[i] < 0) {
-            if (index == -1 || arr[i] < max) {
-                max = arr[i];
-                index = i+1;
+            if (index == -1 || arr[i] < *max) {
+                max = &arr[i];
+                index = i + 1;
             }
         }
     }
     if (index != -1)
     {
-        cout << "Максимальний від'ємний елемент: " << max << endl;
+        cout << "Максимальний від'ємний елемент: " << *max << endl;
         cout << "Індекс елемента: " << index << endl;
     }
     else {
