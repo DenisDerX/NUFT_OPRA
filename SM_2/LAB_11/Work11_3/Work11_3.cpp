e1b97727ffc5ccb867f12f9c2ead49055c90dde8
Denys Derthiy <densichanell@gmail.com>
2023-04-18 16:24:23 +0300

    Hallo

---
+++ SM_2/LAB_11/Work11_3/Work11_3.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    system("cls");
+    setlocale(LC_ALL, "Ukr");
+    srand(time(NULL));
+    const int n = 4;
+    int mat[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            mat[i][j] = rand() % 100-40;
+        }
+    }
+    cout << "Початкова матриця:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i + j == n - 1 && mat[i][j] < 0) {
+                for (int k = 0; k < i; k++) {
+                    if (mat[k][k] < 0 && mat[k][k] < mat[i][j]) {
+                        int temp = mat[k][k];
+                        mat[k][k] = mat[i][j];
+                        mat[i][j] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << endl << "Відсортований масив:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
