e36e100e78475a6cbe2608f520b4bc566dd90a33
Denys Derthiy <densichanell@gmail.com>
2023-04-27 12:45:32 +0300

    Hallo

---
+++ SM_2/LAB_12/Work12_2/Work12_2.cpp
@@ -0,0 +1,33 @@
+﻿#include <time.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+#include <string>
+
+using namespace std;
+
+string cmat(string mat)
+{
+    for (int i = 0; i < mat.size(); i++) 
+    {
+        if (mat[i] == '/') 
+        {
+            mat[i] = '*';
+        }
+    }
+
+    return mat;
+}
+
+int main() 
+{
+    system("cls");
+    setlocale(LC_CTYPE, "UKR");
+    string mat;
+    cout << " Введіть символьний рядок = ";
+    getline(cin, mat);
+    string res = cmat(mat);
+    cout << "Перетворений символьний рядок = " << res << endl;
+    return 0;
+}
